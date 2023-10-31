2247635fd73eb67de81994af4386f23cf7244bd0
toytoy08 <toy31327@gmail.com>
2023-11-01 01:57:55 +0700

    adjust UI, add stat change and generation

--- app/components/Detail/MoveDetail.jsx
+++ app/components/Detail/MoveDetail.jsx
@@ -19,7 +19,7 @@ const MoveDetail = ({ move }) => {
 
     const PokemonNameCard = ({ pokemons = [] }) => (
         <div className="flex flex-col gap-2 border-2 rounded-lg p-4">
-            <h2 className='text-xl font-semibold'>Pokemons</h2>
+            <h2 className='text-xl font-semibold'>Learned by Pokemon</h2>
             <div className="flex flex-wrap gap-2">
                 {pokemons?.map((pokemon, index) => (
                     <Link key={`pokemon-${pokemon.name}`} href={`/pokemons/${pokemon.name}`} className='text-lg capitalize hover:underline hover:underline-offset-4'>
@@ -97,7 +97,7 @@ const MoveDetail = ({ move }) => {
     )
 
     const EffectSection = ({ changes, entries, flavor_text }) => (
-        <div className="w-full flex-1 flex flex-col gap-4 border-2 rounded-lg p-4">
+        <div className="w-full flex flex-col gap-4 border-2 rounded-lg p-4">
             <h2 className='text-3xl font-semibold capitalize'>
                 Effect
             </h2>
@@ -107,12 +107,26 @@ const MoveDetail = ({ move }) => {
         </div>
     )
 
+    const StatChanges = ({ statChanges }) => (
+        <div className="w-full flex flex-col gap-4 border-2 rounded-lg p-4">
+            <h2 className='text-3xl font-semibold capitalize'>
+                Stat Changes
+            </h2>
+            {statChanges.map((stat) => (
+                <div key={stat} className="">
+                    <InfoText title='Stat' value={`${stat.stat.name}`} />
+                    <InfoText title='Changes' value={`${stat.change}`} />
+                </div>
+            ))}
+        </div>
+    )
+
     return (
         <>
             <HeaderText title='Move' value={move.name} />
             {Object.keys(move).length > 0 && (
                 <div className="w-full flex flex-col gap-4">
-                    <div className="w-full flex-1 flex flex-col md:flex-row justify-evenly items-start md:items-center md:divide-x-4 divide-x-0 divide-sky-400 border-2 rounded-lg p-4 mt-4">
+                    <div className="w-full flex flex-wrap flex-col md:flex-row justify-evenly items-start md:items-center gap-2 md:gap-4 md:divide-x-4 divide-x-0 divide-sky-400 border-2 rounded-lg p-4 mt-4">
                         <TypeSection type={move.type.name} />
                         <InfoText title='Power' value={`${move.power}`} />
                         <InfoText title='Accuracy' value={`${move.accuracy}`} />
@@ -120,7 +134,9 @@ const MoveDetail = ({ move }) => {
                         <InfoText title='Damage Class' value={`${move.damage_class.name}`} />
                         <InfoText title='Target' value={`${move.target.name}`} />
                         <InfoText title='Effect Chance' value={`${move.effect_chance}%`} />
+                        <InfoText title='Generation' value={`${move.generation.name}`} />
                     </div>
+                    <StatChanges statChanges={move.stat_changes} />
                     <ContestSection
                         combo={move.contest_combos}
                         type={move.contest_type}
